be1e8fafe1b7214e17c050e2f6fa312d4fd5200f
Mairsmoon <flytothemm08@gmail.com>
2025-01-15 15:11:00 +0900

    Exploratory data project 1

--- plot1.R
+++ plot1.R
@@ -4,39 +4,36 @@
 library(tidyverse)
 
 # load data ---------------------------------------------------------------
-getwd()
-data<-read.table("C:/Users/julie/OneDrive/문서/exdata_data_household_power_consumption/household_power_consumption.txt", 
-                 sep=";", head=TRUE, na.strings = "?")
-object.size(data) ## 읽기 전에 미리 아는 방법? 
-str(data)
+if (!file.exists("UCI EPC Dataset")) {
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(fileUrl, destfile = "UCI EPC Dataset.zip")
+  unzip("UCI EPC Dataset.zip")
+}
 
-##  dates 2007-02-01 and 2007-02-02 사이만 읽기 ?? 
-data.1 <- read.table(pipe("grep '^1/2/2007\\|^2/2/2007' ./exdata_data_household_power_consumption/household_power_consumption.txt"), 
-                     sep = ";", header = FALSE, stringsAsFactors = FALSE)
+data <- read.table("./household_power_consumption.txt", # file path
+                   header = TRUE, 
+                   sep = ";",
+                   na.strings = "?", # read "?" as NA
+                   colClasses = "character") # Read all column as "character"
 
 ## subset dates 
 data.1 <- data[data$Date %in% c("1/2/2007", "2/2/2007"), ]
-str(data)
 
+## make variables into numerics 
 data.2 <- data.1 %>%
-  mutate(Date = dmy(Date), 
-         Time = hms(Time), 
-         Global_active_power = as.numeric(Global_active_power),
-         Global_intensity = as.numeric(Global_intensity), 
-         Sub_metering_1 = as.numeric(Sub_metering_1),
-         Sub_metering_2 = as.numeric(Sub_metering_2))
-
-str(data.2)
-sum(is.na(data.1))
-sum(is.na(data))
+  mutate(DateTime = strptime(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")) %>% 
+  # Combine and edit Date and Time with strptime function
+  mutate(across(Global_active_power:Sub_metering_3, \(x) as.numeric(x)))
 
 
 # make plots --------------------------------------------------------------
-# examine how household energy usage varies over a 2-day period in February, 2007. 
-# for each plot, save it to a PNG file, width 480 pixels and height 480 pixels.
 png(file="plot1.png", width = 480, height = 480)
-hist(data.2$Global_active_power, col="red", main="Global Active Power", 
+
+hist(data.2$Global_active_power, 
+     col="red", 
+     main="Global Active Power", 
      xlab="Global Active Power (kilowatts)")
+
 dev.off()
 
 
